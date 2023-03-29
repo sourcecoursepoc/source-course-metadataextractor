a64a9a0d5598eb28d03821119e443eeef2f499f1
Shiju Kalluvilayil Babu(UST <227588@ust-global.com>
2023-03-29 20:29:19 +0530

    Initial Commit

---
+++ src/main/java/com/ust/sourcecourse/metadataextractor/SourceCourseMetadataExtractorApplication.java
@@ -0,0 +1,13 @@
+package com.ust.sourcecourse.metadataextractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourceCourseMetadataExtractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SourceCourseMetadataExtractorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ust/sourcecourse/metadataextractor/SourceCourseMetadataExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ust.sourcecourse.metadataextractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SourceCourseMetadataExtractorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
