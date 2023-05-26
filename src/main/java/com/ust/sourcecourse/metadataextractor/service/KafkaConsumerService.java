ee54c45e1254fc1add28aaf444892d89eba80507
Chandini Bodigadla <212634@ust-global.com>
2023-05-26 12:44:03 +0530

    homepageapi

---
+++ src/main/java/com/ust/sourcecourse/metadataextractor/service/KafkaConsumerService.java
@@ -0,0 +1,20 @@
+package com.ust.sourcecourse.metadataextractor.service;
+
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.ust.sourcecourse.metadataextractor.constant.AppConstant;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class KafkaConsumerService {
+
+    @KafkaListener(topics = AppConstant.KAFKA_PIPELINE_EVENT_TOPIC,
+            groupId = AppConstant.KAFKA_GROUP_ID)
+    public void consume(String message){
+        log.info(String.format("Message received -> %s", message));
+    }
+}
